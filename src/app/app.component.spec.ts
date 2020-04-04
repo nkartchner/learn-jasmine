d6378811fe983d5f198c4019e17336735d151e4d
nkartchner <39930903+nkartchner@users.noreply.github.com>
2020-04-04 13:23:48 -0600

    made changes to the test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'learn-jasmine'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-jasmine');
+    expect(app.title).toEqual('testing-jasmine');
   });
 
   it('should render title', () => {
