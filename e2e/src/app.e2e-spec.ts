3ce1e289d239243e55f565c4c311a26c86c06164
nkartchner <39930903+nkartchner@users.noreply.github.com>
2020-04-04 13:28:52 -0600

    made changes to the protractor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('learn-jasmine app is running!');
+    expect(page.getTitleText()).toEqual('testing-jasmine app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'learn-jasmine'`, () => {
+  it(`should have as title 'testing-jasmine'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('testing-jasmine');
